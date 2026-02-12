d2e61cd281dc4fc8ae209ab98286538dfe06eddd
Evgenii Petrov IceBook <epoy74@gmail.com>
2026-02-12 12:14:02 +0400

    feat(domain): add error info class

---
+++ domain/error_info.py
@@ -0,0 +1,5 @@
+class ErrorInfo:
+    kind: str  # "transport" | "device"
+    message: str
+    exception_code: int | None = None  # для device, если есть
+    exc_type: str | None = None  # для transport, если есть
