e715dcecd156b379645c3c09b6aa175f8932ee0a
Evgenii Petrov IceBook <epoy74@gmail.com>
2026-02-13 07:59:13 +0000

    chore(polling): Fix funсtion name

--- src/pstg/domain/modbus_device_read_settings.py
+++ src/pstg/domain/modbus_device_read_settings.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class ModbusDeviceReadSennings:
+class ModbusDeviceReadSettings:
     """
     Конфигурация устройства, с которого считываем данные.
 
