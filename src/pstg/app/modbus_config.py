d09fe9deb1429dc0426abfaa730e5ff5c56cf63d
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-02 09:53:54 +0400

    chore(config): WIP hardware test config

--- src/pstg/app/modbus_config.py
+++ src/pstg/app/modbus_config.py
@@ -9,12 +9,12 @@ def get_modbus_config() -> ModbusConfig:
     :rtype: ModbusConfig
     """
     # IP адрес устройства, с которого получаем данные
-    # DEVICE_IP: str = "10.0.6.10"
-    DEVICE_IP: str = "127.0.0.1"
+    DEVICE_IP: str = "10.0.6.10"  # pump station 2326
+    # DEVICE_IP: str = "127.0.0.1"  # local development
 
     #  Порт, через который работаем с устройством
-    # DEVICE_PORT: int = 506
-    DEVICE_PORT: int = 1502
+    DEVICE_PORT: int = 506  # pump station 2326
+    # DEVICE_PORT: int = 1502  # local development
 
     #  Период опроса устройства
     DEVICE_POLL_INTERVAL_S: float = 2
